e32503b69b60274ac7fd946e5d5a79697296230f
ruo2019 <ruo2019@outlook.com>
2022-07-14 16:46:59 -0700

    add GameViewModel class

---
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.android.guesstheword.screens.game
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+class GameViewModel : ViewModel() {
+    init {
+        Log.i("GameViewModel", "GameViewModel created!")
+    }
+}
