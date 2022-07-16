c8dbbf2293610cea05f0a6062fc00e27a19450f0
ruo2019 <ruo2019@outlook.com>
2022-07-16 11:53:08 -0700

    Add the Play Again button

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
@@ -6,17 +6,30 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
-class ScoreViewModel(finalScore : Int): ViewModel() {
+class ScoreViewModel(finalScore: Int) : ViewModel() {
     private var _score = MutableLiveData<Int>()
-    val score : LiveData<Int>
-    get() = _score
+    val score: LiveData<Int>
+        get() = _score
+
+    private var _eventPlayAgain = MutableLiveData<Boolean>()
+    val eventPlayAgain: LiveData<Boolean>
+        get() = _eventPlayAgain
+
     init {
         _score.value = finalScore
         Log.i("ScoreViewModel", "Final score is $finalScore")
     }
+
+    fun onPlayAgain() {
+        _eventPlayAgain.value = true
+    }
+
+    fun onPlayAgainComplete() {
+        _eventPlayAgain.value = false
+    }
 }
 
-class ScoreViewModelFactory(private val finalScore: Int):ViewModelProvider.Factory {
+class ScoreViewModelFactory(private val finalScore: Int) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(ScoreViewModel::class.java)) {
             return ScoreViewModel(finalScore) as T
