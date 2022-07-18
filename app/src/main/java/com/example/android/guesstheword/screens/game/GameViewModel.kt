23329b33c59b466767ac711d3f780b7a81bee0a3
ruo2019 <ruo2019@outlook.com>
2022-07-18 16:08:22 -0700

    Add a timer

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,9 +1,11 @@
 package com.example.android.guesstheword.screens.game
 
+import android.os.CountDownTimer
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.sql.Time
 
 class GameViewModel : ViewModel() {
     // The current word
@@ -16,12 +18,24 @@ class GameViewModel : ViewModel() {
     val score: LiveData<Int>
         get() = _score
 
+    private var _currentTime = MutableLiveData<Long>()
+    val currentTime : LiveData<Long>
+    get() = _currentTime
+
+    private val timer : CountDownTimer
+
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
 
     private var _eventGameFinish = MutableLiveData<Boolean>()
     val eventGameFinish: LiveData<Boolean>
         get() = _eventGameFinish
+
+    companion object {
+        const val DONE = 0L
+        const val SECOND = 1000L
+        const val TIME = 60000L
+    }
 
     /**
      * Resets the list of words and randomizes the order
@@ -59,6 +73,17 @@ class GameViewModel : ViewModel() {
         resetList()
         nextWord()
         Log.i("GameViewModel", "GameViewModel created!")
+        timer = object : CountDownTimer(TIME, SECOND) {
+            override fun onTick(p0: Long) {
+                _currentTime.value = p0/ SECOND
+            }
+
+            override fun onFinish() {
+                _currentTime.value = DONE
+                onGameFinish()
+            }
+        }
+        timer.start()
     }
 
     fun onGameFinish() {
@@ -78,7 +103,7 @@ class GameViewModel : ViewModel() {
             _word.value = wordList.removeAt(0)
         }
         else {
-            onGameFinish()
+            resetList()
         }
     }
 
@@ -95,6 +120,6 @@ class GameViewModel : ViewModel() {
 
     override fun onCleared() {
         super.onCleared()
-        Log.i("GameViewModel", "GameViewModel destroyed!")
+        timer.cancel()
     }
 }
